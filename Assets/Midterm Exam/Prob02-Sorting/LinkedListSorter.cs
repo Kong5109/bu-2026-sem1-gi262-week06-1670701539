8a246ebca521ff91970ec917f9bf57fe454a1548
Kong5109 <kong8071.kk@gmail.com>
2026-09-22 13:43:37 +0700

    prob 1&2

--- Assets/Midterm Exam/Prob02-Sorting/LinkedListSorter.cs
+++ Assets/Midterm Exam/Prob02-Sorting/LinkedListSorter.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.GraphToolkit.Editor;
 using UnityEngine;
 
 namespace MidtermExam.Prob02
@@ -13,6 +14,35 @@ namespace MidtermExam.Prob02
         public LinkedList<int> SortAscending(LinkedList<int> list)
         {
             // TODO: Implement sorting algorithm for LinkedList<int> (Ascending)
+            if (list == null)
+            {
+                return null;
+            }
+            if (list.Count == 1)
+            {
+                return list;
+            }
+
+            LinkedListNode<int> currentNode = list.First;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                LinkedListNode<int> minNode = currentNode;
+                LinkedListNode<int> checkNode = currentNode;
+                while (checkNode.Next != null)
+                {
+                    checkNode = checkNode.Next;
+                    if (checkNode.Value < minNode.Value)
+                    {
+                        minNode = checkNode;
+                    }
+                }
+
+                int temp = currentNode.Value;
+                currentNode.Value = minNode.Value;
+                minNode.Value = temp;
+
+                currentNode = currentNode.Next;
+            }
             return list;
         }
 
@@ -24,6 +54,35 @@ namespace MidtermExam.Prob02
         public LinkedList<int> SortDescending(LinkedList<int> list)
         {
             // TODO: Implement sorting algorithm for LinkedList<int> (Descending)
+            if (list == null)
+            {
+                return null;
+            }
+            if (list.Count == 1)
+            {
+                return list;
+            }
+
+            LinkedListNode<int> currentNode = list.First;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                LinkedListNode<int> maxNode = currentNode;
+                LinkedListNode<int> checkNode = maxNode;
+                while (checkNode.Next != null)
+                {
+                    checkNode = checkNode.Next;
+                    if (maxNode.Value < checkNode.Value)
+                    {
+                        maxNode = checkNode;
+                    }
+                }
+
+                int temp = currentNode.Value;
+                currentNode.Value = maxNode.Value;
+                maxNode.Value = temp;
+
+                currentNode = currentNode.Next;
+            }
             return list;
         }
     }
